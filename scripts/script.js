6f1baf79c38d9f4e75deb9f7527460000ce5d31d
Ken HIll <ken.hill30@gmail.com>
2020-07-15 20:45:44 -0400

    Added careers.html page and began styling. Added image to careers.html page. Fixed styling bugs

--- scripts/script.js
+++ scripts/script.js
@@ -10,7 +10,7 @@ const navSlide = () => {
         if(link.style.animation) {
           link.style.animation = ``;
         } else {
-          link.style.animation = `navLinkFade 0.5s ease forwards ${index / 6 + 0.2}s`;
+          link.style.animation = `navLinkFade 0.3s ease forwards ${index / 20 + 0.2}s`;
         }
       });
       burger.classList.toggle('toggle');
